f45c675b7bf878ecbe07b22918244d77b4c67665
cblystone67 <christopherblystone1@gmail.com>
2024-02-26 12:31:09 -0800

    Added a loop

--- mypython.py
+++ mypython.py
@@ -3,10 +3,23 @@ import turtle
 a = 100
 
 t = turtle.Turtle()
+t.color = ("blue")
 
-t.forward(a)
-t.right(120)
-t.forward(a)
-t.right(120)
-t.forward(a)
-t.right(120)
+"""for <loop variable> in <sequence>:
+      # do repeated code 
+This is a for loop.  Loop Variable: variable that gets assigned the next value in sequence at every repetition of the loop
+Sequence: can be either Characters in a string, Range of numbers, Items in a list, Lines in a file.
+Repeated code: code that gets executed at every repetition of the loop, likely uses the loop variable."""
+for i in range(3):
+    t.forward(a)
+    t.right(120)
+
+"""Functions: Named group of code statements
+Makes your program easier to read, test, and debut
+Simplify testing and debugging
+test & debug simpler individual functions one at a time before testing your program is working as a whole.
+Reuse!
+Once written & debugged, can be reused
+Eliminate repetitive code
+Make changes in only one place (DRY)
+"""
