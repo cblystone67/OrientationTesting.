b6d71bd3405739824fcadd0abf77e100d3f12da8
cblystone67 <christopherblystone1@gmail.com>
2024-02-28 14:37:31 -0800

    fixed madfun.py

--- madfun.py
+++ madfun.py
@@ -1,22 +1,10 @@
 # Christopher Blystone
 # Collect a decimal number and give the absolute value, rounded and square root.
 import math
-from math import sqrt
+
 
 num = float(input("Please give me a decimal number: "))
 
-
-def madfun(number):
-    absNum = abs(number)
-    roundNum = round(number)
-    if (number < 0):
-        number = number * -1
-        squrNum = sqrt(number)
-    else:
-        squrNum = sqrt(number)
-    print(absNum)
-    print(roundNum)
-    print(squrNum)
-
-
-madfun(num)
+print("absolute value: ", abs(num))
+print("rounded to 0: ", round(num, 0))
+print("square root: ", math.sqrt(abs(round(num, 0))))
