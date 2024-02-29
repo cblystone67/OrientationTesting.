768ad85796c5de1927c8712f1388d83ed19875a8
cblystone67 <christopherblystone1@gmail.com>
2024-02-29 05:54:43 -0800

    Finished Loops module 3

---
+++ module_3.py
@@ -0,0 +1,36 @@
+# Christopher Blystone
+# For Loop Programs
+
+# Problem 1
+# A.
+for i in range(1, 6):
+    print(i)
+print("End of A.")
+print()
+# B.
+for i in range(2, 12, 3):
+    print(i)
+print("End of B")
+print()
+# C.
+for i in range(-10, 1, 2):
+    print(i, end=" ")
+print("End of C")
+print()
+# D.
+star = "****"
+for _ in range(4):
+    print(star)
+print("End of D")
+print()
+# 2. Write a for loop that prints the letters in "CSCI 150" on separate lines.
+word = "CSCI 150"
+for i in word:
+    print(i)
+print("End of 2.")
+print()
+# 3 Using the range function, print the numbers from 1 to 10 (inclusive). Each number should be on a separate line.
+for i in range(1, 11):
+    print(i)
+print("End of 3")
+print()
