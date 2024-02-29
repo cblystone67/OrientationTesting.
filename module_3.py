58c56ed6b7bf135545b0e325dd4cfffe22ebd570
cblystone67 <christopherblystone1@gmail.com>
2024-02-29 06:17:09 -0800

    Finished Loops module 3

--- module_3.py
+++ module_3.py
@@ -21,6 +21,13 @@ print()
 star = "****"
 for _ in range(4):
     print(star)
+
+print()
+
+for i in range(4):  # lines
+    for j in range(4):  # columns for each row
+        print("*", end="")
+    print()
 print("End of D")
 print()
 # 2. Write a for loop that prints the letters in "CSCI 150" on separate lines.
@@ -28,6 +35,10 @@ word = "CSCI 150"
 for i in word:
     print(i)
 print("End of 2.")
+print()
+# her form
+for c in "CSCI 150":
+    print(c)
 print()
 # 3 Using the range function, print the numbers from 1 to 10 (inclusive). Each number should be on a separate line.
 for i in range(1, 11):
