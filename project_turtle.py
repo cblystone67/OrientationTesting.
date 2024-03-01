ade27e048becc0d2f63bcf09ac2dca08b7fd145d
cblystone67 <christopherblystone1@gmail.com>
2024-03-01 06:15:19 -0800

    Creating a Turtle Program

---
+++ project_turtle.py
@@ -0,0 +1,9 @@
+# Christopher Blystone
+# 3/1/24
+# Project Turtle.
+import turtle
+# Create a line.
+turtle.forward(50)
+
+
+turtle.Screen().exitonclick()
--- pywriting.py
+++ pywriting.py
@@ -14,3 +14,27 @@ print(phrase.title())
 print(phrase.count("o"))
 print(phrase.lower().islower())
 print(phrase.isdecimal())
+
+# Reading If's
+# 2
+DoW = input("What day is it? ").title()
+
+if DoW == "Saturday":
+    print("Wake up at 9 am")
+elif DoW == "Sunday":
+    print("Wake up at 10 am")
+else:
+    print("Wake up at 7 am")
+
+x = 3
+y = 7
+
+print(not x == 5)
+print(x > 0 and y > 0)
+print(x < 0 or y < 0)
+print(x*y > 20 or x*y < -20)
+
+print(not (x == 0 or y == 5))
+print(not x == 0 or y == 5)
+print(x == 3 and not y == 5)
+print(not (x == 3 and y == 5))
