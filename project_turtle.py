1598541b1c2194df490fd5275d37aed9d9018395
cblystone67 <christopherblystone1@gmail.com>
2024-03-01 06:20:05 -0800

    created a squzre function

--- project_turtle.py
+++ project_turtle.py
@@ -2,8 +2,18 @@
 # 3/1/24
 # Project Turtle.
 import turtle
+# Creating a turtle shape
+turtle.shape("turtle")
+# Changing the speed of the turtle drawing.
+turtle.speed(0)
+
+
 # Create a line.
-turtle.forward(50)
+def square(len):
+    for i in range(4):
+        turtle.forward(len)
+        turtle.right(90)
 
 
+square(100)
 turtle.Screen().exitonclick()
